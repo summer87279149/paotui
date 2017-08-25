87606da6c63dbb2decc15b58eea28d33256630a8
吕志杰 <lvzhijie@lvzhijiedeMacBook-Air.local>
2017-08-25 11:39:45 +0800

    描述信息

---
+++ Running/lzj_view.h
@@ -0,0 +1,13 @@
+//
+//  lzj_view.h
+//  Running
+//
+//  Created by 吕志杰 on 2017/8/25.
+//  Copyright © 2017年 吕志杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lzj_view : UIView
+
+@end
---
+++ Running/lzj_view.m
@@ -0,0 +1,21 @@
+//
+//  lzj_view.m
+//  Running
+//
+//  Created by 吕志杰 on 2017/8/25.
+//  Copyright © 2017年 吕志杰. All rights reserved.
+//
+
+#import "lzj_view.h"
+
+@implementation lzj_view
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
